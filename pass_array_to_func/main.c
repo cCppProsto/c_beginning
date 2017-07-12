b3b3cf781bfd1eceeedb6709d97503d1345260b8
ioleshko <ioleshko@ioleshko>
2017-07-12 10:06:27 +0300

    was added two dimensional array

--- pass_array_to_func/main.c
+++ pass_array_to_func/main.c
@@ -168,6 +168,152 @@ void one_dimensional_array_example4()
   print(arr_3);
 }
 
+/*------------------- EXAMPLE 1 ----------------------------------------------*/
+void two_dimensional_array_example1()
+{
+  //   |0|1|2|
+  // 0 [ , , ]
+  // 1 [ , , ]
+  int arr_1[2][3] = { {1,2,3}, {4,5,6} }; // 0x?? 1 2 3 4 5 6
+  int arr_2[][3]  = { {1,2,3}, {4,5,6} }; // 0x?? 1 2 3 4 5 6
+  //int arr_3[][]  = { {1,2,3}, {4,5,6} }; // error
+
+  void print(int *aArray, int aLines, int aColumns)
+  {
+    for(int l = 0; l < aLines; ++l)
+    {
+      for(int c = 0; c < aColumns; ++c)
+        printf("%i, ", aArray[l*aColumns+c]);
+      printf("\n");
+    }
+  }
+
+  int get_v(const int *aArray, int aColumns, int aLine, int aColumn)
+  {
+    return aArray[aLine*aColumns + aColumn];
+  }
+
+  void set_v(int *aArray, int aColumns, int aLine, int aColumn, int aValue)
+  {
+    aArray[aLine*aColumns + aColumn] = aValue;
+  }
+
+  //printf("%x\n", &arr_1);
+  //printf("%x\n", &arr_1[0]);
+  //printf("%x\n", &arr_1[0][0]);
+
+  //print(arr_1, 2, 3);         // warning
+  //print(&arr_1[0], 2, 3);     // warning
+  print(&arr_1[0][0], 2, 3);
+
+  printf("%i\n", get_v(&arr_1[0][0], 3, 1, 1));
+  set_v(&arr_1[0][0], 3, 1, 1, 100);
+  printf("%i\n", get_v(&arr_1[0][0], 3, 1, 1));
+}
+
+/*------------------- EXAMPLE 2 ----------------------------------------------*/
+void two_dimensional_array_example2()
+{
+  int arr_1[2][3] = { {1,2,3}, {4,5,6} }; // 0x?? 1 2 3 4 5 6
+  int arr_2[][3]  = { {1,2,3}, {4,5,6} }; // 0x?? 1 2 3 4 5 6
+
+  void print_a23(int aArray[2][3])
+  {
+    for(int l = 0; l < 2; ++l)
+    {
+      for(int c = 0; c < 3; ++c)
+        printf("%i, ", aArray[l][c]);
+      printf("\n");
+    }
+  }
+
+  int get_v_a23(const int aArray[2][3], int aLine, int aColumn)
+  {
+    return aArray[aLine][aColumn];
+  }
+
+  void set_v_a23(int aArray[2][3], int aLine, int aColumn, int aValue)
+  {
+    aArray[aLine][aColumn] = aValue;
+  }
+
+  print_a23(arr_1);
+  print_a23(arr_2);
+
+  printf("%i\n", get_v_a23(arr_1, 1, 1));
+  set_v_a23(arr_1, 1, 1, 100);
+  printf("%i\n", get_v_a23(arr_1, 1, 1));
+}
+/*------------------- EXAMPLE 3 ----------------------------------------------*/
+void two_dimensional_array_example3()
+{
+  int arr_1[2][3] = { {1,2,3}, {4,5,6} };           // 0x?? 1 2 3 4 5 6
+  int arr_2[][3]  = { {1,2,3}, {4,5,6}, {7,8,9} };  // 0x?? 1 2 3 4 5 6 7 8 9
+
+  void print_al3(int aArray[][3], int aLines)
+  {
+    for(int l = 0; l < aLines; ++l)
+    {
+      for(int c = 0; c < 3; ++c)
+        printf("%i, ", aArray[l][c]);
+      printf("\n");
+    }
+  }
+
+  int get_v_al3(const int aArray[][3], int aLine, int aColumn)
+  {
+    return aArray[aLine][aColumn];
+  }
+
+  void set_v_al3(int aArray[][3], int aLine, int aColumn, int aValue)
+  {
+    aArray[aLine][aColumn] = aValue;
+  }
+
+  print_al3(arr_1, 2);
+  print_al3(arr_2, 3);
+
+  printf("%i\n", get_v_al3(arr_1, 1, 1));
+  set_v_al3(arr_1, 1, 1, 100);
+  printf("%i\n", get_v_al3(arr_1, 1, 1));
+}
+/*------------------- EXAMPLE 4 ----------------------------------------------*/
+void two_dimensional_array_example4()
+{
+  int arr_1[2][3] = { {1,2,3}, {4,5,6} };           // 0x?? 1 2 3 4 5 6
+
+  void print_arr(int **aArray, int aLines, int aColumns)
+  {
+    for(int l = 0; l < aLines; ++l)
+    {
+      for(int c = 0; c < aColumns; ++c)
+      {
+        printf("%i\n", aArray[l*3 + c]);
+        printf("%i\n", *(aArray + (l*3 + c)));
+
+        printf("%i\n", aArray[l*3 + c + 1]);
+        printf("%i\n", *((int *)aArray + (l*3 + c + 1)));
+
+        printf("%i\n", aArray[l*3 + c + 2]);
+        printf("%i\n", *((int *)aArray + (l*3 + c + 2)));
+
+        //int **pp;  // pp++
+        //int *p;    // p++
+
+        //printf("%i, ", aArray[l][c]);         // = *(*(aArray+l) + c)
+      }
+      printf("\n");
+    }
+  }
+
+  print_arr((int**)arr_1, 2, 3);
+
+  print_arr(&arr_1[0][0], 2, 3);
+  print_arr(&arr_1[0], 2, 3);
+  print_arr(&arr_1, 2, 3);
+  print_arr(arr_1[0], 2, 3);
+  print_arr(arr_1[0][0], 2, 3);
+}
 
 int main()
 {
@@ -196,11 +342,13 @@ int main()
   tmp = *(*a3);
   tmp = a4[0];
 
-
-
-
-
   one_dimensional_array_example4();
+
+
+  two_dimensional_array_example1();
+  two_dimensional_array_example2();
+  two_dimensional_array_example3();
+  two_dimensional_array_example4();
 
   return 0;
 }
